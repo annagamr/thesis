19bd219a7db06e486633777097ce487826859de1
Ani Gamreklidze <gamreklidzeanna@gmail.com>
2023-03-05 23:40:47 +0100

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,34 @@
-import logo from './logo.svg';
 import './App.css';
+import Header from './components/header/header'
+import Footer from './components/footer/footer'
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import About from "./components/about/about";
+import Shop from "./components/shop/shop";
+import Blog from "./components/blog/blog";
+import Register from "./components/register/register";
+import Cart from "./components/cart/cart";
+import ContactPage from "./components/contact/contact";
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+      <Router>
+        <Header />
+        <Routes>
+          <Route path="/about" element={<About />} />
+          <Route path="/shop" element={<Shop />} />
+          <Route path="/blog" element={<Blog />} />
+          <Route path="/register" element={<Register />} />
+          <Route path="/cart" element={<Cart />} />
+          <Route path="/contact" element={<ContactPage />} />
+
+
+        </Routes>
+        <Footer />
+
+      </Router>
     </div>
   );
 }
---
+++ src/components/about/about.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import Cover from './cover.jpg'
+import './about.css'
+
+
+const About= () => {
+  return (
+    <div className='upper_body'>
+      {/* <img src={Cover} alt="" /> */}
+    </div>
+  )
+}
+export default About;
