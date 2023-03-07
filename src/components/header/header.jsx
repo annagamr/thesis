0fb738e1e9b40272e7591ba051b5aa5103bc8971
Ani Gamreklidze <gamreklidzeanna@gmail.com>
2023-03-07 19:40:25 +0100

    registration page initial

--- src/components/header/header.jsx
+++ src/components/header/header.jsx
@@ -4,28 +4,27 @@ import { BsCart4 } from "react-icons/bs";
 import { RxPerson } from "react-icons/rx";
 import { MdQuestionAnswer } from "react-icons/md";
 import { IconContext } from "react-icons";
-import Badge from '@mui/material/Badge';
+import Badge from "@mui/material/Badge";
 import { Link } from "react-router-dom";
 
 const Header = () => {
   return (
     <header className="top_header">
       <ul className="header_items_left">
-        
         <li>
-          <Link to="/about">About</Link>
+          <Link to="/about">ABOUT</Link>
         </li>
         <li>
-          <Link to="/shop">Shop</Link>
+          <Link to="/shop">SHOP</Link>
         </li>
         <li>
-          <Link to="/blog">Blog</Link>
+          <Link to="/blog">BLOG</Link>
         </li>
       </ul>
       <div className="logo_aurora">Aurora.</div>
       <ul className="header_items_right">
         <li>
-          <Link to='/register' className="icon">
+          <Link to="/register" className="icon">
             <IconContext.Provider
               value={{ color: "white", size: "20px", verticalAlign: "middle" }}
             >
@@ -34,9 +33,8 @@ const Header = () => {
           </Link>
         </li>
         <li>
-          <Link to='/cart' className="icon">
-          <Badge badgeContent={1} color="success">
-
+          <Link to="/cart" className="icon">
+            <Badge badgeContent={1} color="success">
               <IconContext.Provider
                 value={{
                   color: "white",
@@ -46,11 +44,11 @@ const Header = () => {
               >
                 <BsCart4 />
               </IconContext.Provider>
-              </Badge>
+            </Badge>
           </Link>
         </li>
         <li>
-          <Link to='/contact' className="icon">
+          <Link to="/contact" className="icon">
             <IconContext.Provider
               value={{ color: "white", size: "20px", verticalAlign: "middle" }}
             >
--- src/components/register/register.jsx
+++ src/components/register/register.jsx
@@ -1,8 +1,39 @@
-import React from 'react'
+import React from "react";
+import "./register.css";
+import { Link } from "react-router-dom";
+import shop from "./shop1.png";
+import user from "./user.png";
 
-const Register= () => {
+const Register = () => {
   return (
-    <div>fdfaf</div>
-  )
-}
+    <div className="register-page">
+      <div className="register-header">
+        I would like to...
+      </div>
+    <div className="choices-container">
+      <div className="create-shop">
+        <div className="top-image">
+          <img src={shop} alt="" />
+        </div>
+        <div className="btn">
+          <Link to="/shop-registration" className="btn1">
+            Create Shop
+          </Link>
+        </div>
+      </div>
+      <div className="become-user">
+        <div className="top-image">
+          <img src={user} alt="" />
+        </div>
+        <div className="btn">
+          <Link to="/user-registration" className="btn2">
+            Become User
+          </Link>
+        </div>
+      </div>
+    </div>
+    </div>
+
+  );
+};
 export default Register;
