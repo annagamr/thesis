cf9ea1f51c5ace6017568b5fad593c9dcc0d909b
Ani Gamreklidze <gamreklidzeanna@gmail.com>
2023-03-07 20:53:55 +0100

    register page drafted, home page slider started

--- src/components/about/about.jsx
+++ src/components/about/about.jsx
@@ -1,13 +1,59 @@
-import React from 'react'
-import Cover from './cover.jpg'
-import './about.css'
+import React from "react";
+import Cover from "./2.jpg";
+import "./about.css";
+import {
+  MdKeyboardDoubleArrowLeft,
+  MdKeyboardDoubleArrowRight,
+} from "react-icons/md";
 
-
-const About= () => {
+const About = () => {
   return (
-    <div className='upper_body'>
-      {/* <img src={Cover} alt="" /> */}
+    <div className="slider-body">
+      <div className="slider-arrows">
+        <MdKeyboardDoubleArrowLeft className="left-arrow" />
+        <div className="wrapper">
+          <div className="slide">
+            <div className="imgcontainer">
+              <img src={Cover} alt="" />
+            </div>
+            <div className="titlecontainer">
+              <div className="title">
+                Rekindle your love for your skin with Re-commence
+              </div>
+              <div className="desc">
+                Welcome to Aurora, the ultimate destination for rejuvenating
+                skincare products. Our online store offers a range of specially
+                curated skincare essentials that help you rediscover your
+                natural radiance. With Aurora, you can confidently embrace the
+                journey of re-commencing your skincare routine and start loving
+                your skin again.
+              </div>
+              <button className="btn-discover">DISCOVER</button>
+            </div>
+          </div>
+          <div className="slide">
+            <div className="imgcontainer">
+              <img src={Cover} alt="" />
+            </div>
+            <div className="titlecontainer">
+              <div className="title">
+                Rekindle your love for your skin with Re-commence
+              </div>
+              <div className="desc">
+                Welcome to Aurora, the ultimate destination for rejuvenating
+                skincare products. Our online store offers a range of specially
+                curated skincare essentials that help you rediscover your
+                natural radiance. With Aurora, you can confidently embrace the
+                journey of re-commencing your skincare routine and start loving
+                your skin again.
+              </div>
+              <button className="btn-discover">DISCOVER</button>
+            </div>
+          </div>
+        </div>
+        <MdKeyboardDoubleArrowRight className="right-arrow" />
+      </div>
     </div>
-  )
-}
+  );
+};
 export default About;
