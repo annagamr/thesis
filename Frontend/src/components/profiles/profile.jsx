dc616712518820a98277c473219a3f5237d63bfa
Ani Gamreklidze <gamreklidzeanna@gmail.com>
2023-04-03 23:44:52 +0200

    seller profile started

--- Frontend/src/components/profiles/profile.jsx
+++ Frontend/src/components/profiles/profile.jsx
@@ -1,6 +1,6 @@
-import React, {useState, useEffect} from "react";
+import React, { useState, useEffect } from "react";
 import AuthService from "../../services/auth.service";
-import "./profile.css"
+import "./profile.css";
 const Profile = () => {
   const [sellerProf, setShowSellerProfile] = useState(false);
   const [adminProf, setShowAdminProfile] = useState(false);
@@ -14,15 +14,24 @@ const Profile = () => {
       setShowUserProfile(user.roles.includes("ROLE_USER"));
       setShowSellerProfile(user.roles.includes("ROLE_SELLER"));
       setShowAdminProfile(user.roles.includes("ROLE_ADMIN"));
-
     }
   }, []);
   return (
     <div className="profile-page">
-  {sellerProf && (
-currentUser.username
-  )}
-  </div>)
+      {sellerProf && <div className="profile-card">
+      <h1 className="profile-title">My Account</h1>
+      <div className="profile-picture">
+        <img src="" alt="" />
+      </div>
+      <div className="info">
+      <div className="name">Shop Name: {currentUser.username}</div>
+      <div className="email">Email: {currentUser.email}</div>
+      <div className="num">Listed Products: 0</div>
+      </div>
+      <button className="edit">Edit Profile</button>
+      </div>}
+    </div>
+  );
 };
 
 export default Profile;
