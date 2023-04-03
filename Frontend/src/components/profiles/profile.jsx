c1a7952fbb75021df8caf5876168c15e323f711b
Ani Gamreklidze <gamreklidzeanna@gmail.com>
2023-04-04 00:43:35 +0200

    seller profile finished

--- Frontend/src/components/profiles/profile.jsx
+++ Frontend/src/components/profiles/profile.jsx
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from "react";
 import AuthService from "../../services/auth.service";
-import "./profile.css";
+import "./profile.css"
+import pic from "./bag.png"
 const Profile = () => {
   const [sellerProf, setShowSellerProfile] = useState(false);
   const [adminProf, setShowAdminProfile] = useState(false);
@@ -18,10 +19,14 @@ const Profile = () => {
   }, []);
   return (
     <div className="profile-page">
-      {sellerProf && <div className="profile-card">
+      {sellerProf && 
+      <div>
+      <div className="profile-illustration">
+      </div>
+      <div className="profile-card">
       <h1 className="profile-title">My Account</h1>
       <div className="profile-picture">
-        <img src="" alt="" />
+        <img src={pic} alt="" />
       </div>
       <div className="info">
       <div className="name">Shop Name: {currentUser.username}</div>
@@ -29,8 +34,10 @@ const Profile = () => {
       <div className="num">Listed Products: 0</div>
       </div>
       <button className="edit">Edit Profile</button>
+      </div>
       </div>}
     </div>
+    
   );
 };
 
