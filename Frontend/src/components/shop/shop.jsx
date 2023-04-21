66629e0eb3ba92c8cfdf5a8fde9903e8eabda5f1
Ani Gamreklidze <gamreklidzeanna@gmail.com>
2023-04-21 16:21:09 +0200

    shop design

--- Frontend/src/components/shop/shop.jsx
+++ Frontend/src/components/shop/shop.jsx
@@ -1,9 +1,18 @@
 import React, { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
 import axios from "axios";
+import "./shop.css";
 
 const Shop = () => {
   const [products, setProducts] = useState([]);
+  const [addedProducts, setAddedProducts] = useState({});
+
+  const handleAddClick = (productId) => {
+    setAddedProducts({
+      ...addedProducts,
+      [productId]: !addedProducts[productId],
+    });
+  };
 
   useEffect(() => {
     const fetchProducts = async () => {
@@ -19,17 +28,42 @@ const Shop = () => {
 
   return (
     <div className="shop-page-container">
-      {products.map((product) => (
-        <Link key={product.id} to={`/product/${product.id}`}>
-          <div className="myCard" key={product.id}>
-            <div className="card-image">
-              <img src={"http://localhost:3002/" + product.image} alt="" />
+      <div className="LayoutGrid grid-cols-12 has-column-gap gap-y-6 sm:gap-y-12">
+        {products.map((product) => (
+          <Link
+            style={{ textDecoration: "none", color: "black" }}
+            key={product.id}
+            to={`/product/${product.id}`}
+          >
+            <div className="LayoutCard" key={product.id}>
+              <div className="product-image-button-container">
+                <div className="product-image">
+                  <img
+                    src={"http://localhost:3002/" + product.image}
+                    alt="Product Image"
+                  />
+                </div>
+                <button
+                  className="add-tocart"
+                  onClick={(e) => {
+                    e.preventDefault();
+                    handleAddClick(product.id);
+                  }}
+                >
+                  {addedProducts[product.id] ? "Added" : "Add"}{" "}
+                  <span className="add-tocart-symbol">
+                    {addedProducts[product.id] ? "✓" : "+"}
+                  </span>
+                </button>
+              </div>
+              <div className="details">
+                <p className="product-title">{product.title}</p>
+                <p className="product-price">{product.price} HUF</p>
+              </div>
             </div>
-            <p className="card-title">{product.title}</p>
-            <p className="card-price">Price: {product.price} HUF</p>
-          </div>
-        </Link>
-      ))}
+          </Link>
+        ))}
+      </div>
     </div>
   );
 };
