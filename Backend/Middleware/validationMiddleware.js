c147ff5d8097bc0fbe4ed0fea3d30f1d02ea92da
Ani Gamreklidze <gamreklidzeanna@gmail.com>
2023-05-04 17:15:45 +0200

    middleware testing finished

--- Backend/Middleware/validationMiddleware.js
+++ Backend/Middleware/validationMiddleware.js
@@ -11,7 +11,7 @@ const checkDuplicateUsernameOrEmailAndRolesExisted = async (req, res, next) => {
 
     const userByEmail = await db.user.findOne({ email: req.body.email });
     if (userByEmail) {
-      console.log("email exists")
+      // console.log("email exists")
 
       return res.status(400).send({ message: "Failed! Email is already in use!" });
 
@@ -20,7 +20,7 @@ const checkDuplicateUsernameOrEmailAndRolesExisted = async (req, res, next) => {
     if (req.body.roles) {
       for (let i = 0; i < req.body.roles.length; i++) {
         if (!db.ROLES.includes(req.body.roles[i])) {
-          console.log("role does not exists")
+          // console.log("role does not exists")
           return res.status(400).send({
             message: `Failed! Role ${req.body.roles[i]} does not exist!`
           });
