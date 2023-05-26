87cc5cec8b136a7b1a6c1aedb20124e3dd0c36a0
Ani Gamreklidze <gamreklidzeanna@gmail.com>
2023-05-26 20:43:58 +0200

    update

--- Frontend/src/services/product.service.js
+++ Frontend/src/services/product.service.js
@@ -6,15 +6,15 @@ class ProductService {
 
   }
   getSellerProducts(userId) {
-    return axios.get(`http://localhost:3002/api/productsbyAuthor/${userId}`);
+    return axios.get(process.env.REACT_APP_BACKEND_ENDPOINT + `/api/productsbyAuthor/${userId}`);
 
   }
   getProductImages(userId) {
-    return axios.get(`http://localhost:3002/api/product-image/${userId}`);
+    return axios.get(process.env.REACT_APP_BACKEND_ENDPOINT + `/api/product-image/${userId}`);
   }
   async getProductById(id) {
     try {
-      const response = await axios.get(`http://localhost:3002/api/products/${id}`);
+      const response = await axios.get(process.env.REACT_APP_BACKEND_ENDPOINT + `/api/products/${id}`);
       return response.data;
     } catch (error) {
       console.log(error);
